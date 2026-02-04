9e4e804abdbe1ab7293b324863467c969006f837
h8d13 <hadrien.loge@gmail.com>
2026-02-04 03:17:29 +0100

    rem

--- hello.sh
+++ hello.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo "Hello, $1!"
 # This is a long comment that explains what this function does
-echo "Args: $@" # another long comment on a line (was 107 bytes)
+echo "Args: $@" # another long comment on a line (was 107 bytes) that will not be stripped since its not a single line
+
+# inspired by zuzzuc and bashx
